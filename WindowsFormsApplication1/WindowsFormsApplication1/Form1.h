5fd3a3586dc90267c48309b3ea6403d54e5ec556
Majaq <maciej.majewski19@op.pl>
2015-05-29 16:47:53 +0200

    Logowanie

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.h
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.h
@@ -34,6 +34,13 @@ namespace WindowsFormsApplication1 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^  label1;
+	protected: 
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::TextBox^  textBox2;
+	private: System::IO::FileSystemWatcher^  fileSystemWatcher1;
 
 	private:
 		/// <summary>
@@ -48,13 +55,95 @@ namespace WindowsFormsApplication1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Form1";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->fileSystemWatcher1 = (gcnew System::IO::FileSystemWatcher());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->fileSystemWatcher1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(61, 83);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(33, 13);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Login";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(61, 149);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(36, 13);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"Has³o";
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(141, 211);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(123, 23);
+			this->button1->TabIndex = 2;
+			this->button1->Text = L"Zaloguj";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(141, 80);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(123, 20);
+			this->textBox1->TabIndex = 3;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(141, 149);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(123, 20);
+			this->textBox2->TabIndex = 4;
+			// 
+			// fileSystemWatcher1
+			// 
+			this->fileSystemWatcher1->EnableRaisingEvents = true;
+			this->fileSystemWatcher1->SynchronizingObject = this;
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(386, 328);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Name = L"Form1";
+			this->Text = L"Form1";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->fileSystemWatcher1))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
+	
+		private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+					 textBox1->FormClosing += gcnew FormClosingEventHandler (this, &Form1::textBox1_FormClosing);
+					 textBox2->PasswordChar='*';
+
+					 textBox1_FormClosing(){
+						 if(((Form^)sender)->Controls[0]->text != "ftims")
+							 e->Cancel=true;
+						 else
+							 e->Cancel=false;					
+						 }
+
+
+			 }
 	};
 }
+
 
